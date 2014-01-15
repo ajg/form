319705683a5638f6270a6e710f8e51cd188fedac
Alvaro J. Genial <genial@alva.ro>
2014-01-14 20:49:02 -0500

    Add LICENSE and factor out documentation

--- decode.go
+++ decode.go
@@ -1,4 +1,7 @@
-// Package form implements encoding and decoding of application/x-www-form-urlencoded data.
+// Copyright 2013 Alvaro J. Genial. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package form
 
 import (
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2013 Alvaro J. Genial. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package form implements encoding and decoding of application/x-www-form-urlencoded data.
+package form

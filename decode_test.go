0daa018f71f55ac9c1c4982313e851c1fb65e9bd
Alvaro J. Genial <genial@alva.ro>
2014-01-15 14:40:37 -0500

    Update year to 2014

--- decode_test.go
+++ decode_test.go
@@ -1,4 +1,4 @@
-// Copyright 2013 Alvaro J. Genial. All rights reserved.
+// Copyright 2014 Alvaro J. Genial. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 

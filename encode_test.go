75a8fa2c9b6bc6a44ba7a329b23cad2b409941f0
Alvaro J. Genial <genial@alva.ro>
2016-08-02 15:21:09 -0400

    Implement Encoder.KeepZeros

--- encode_test.go
+++ encode_test.go
@@ -43,3 +43,59 @@ func TestEncode(t *testing.T) {
 		}
 	}
 }
+
+type Thing1 struct {
+	String  string `form:"name,omitempty"`
+	Integer *uint  `form:"num,omitempty"`
+}
+
+type Thing2 struct {
+	String  string `form:"name,omitempty"`
+	Integer uint   `form:"num,omitempty"`
+}
+
+type Thing3 struct {
+	String  string `form:"name"`
+	Integer *uint  `form:"num"`
+}
+
+type Thing4 struct {
+	String  string `form:"name"`
+	Integer uint   `form:"num"`
+}
+
+func TestEncode_KeepZero(t *testing.T) {
+	num := uint(0)
+	for _, c := range []struct {
+		b interface{}
+		s string
+		z bool
+	}{
+		{Thing1{"test", &num}, "name=test&num=", false},
+		{Thing1{"test", &num}, "name=test&num=0", true},
+		{Thing2{"test", num}, "name=test", false},
+		{Thing2{"test", num}, "name=test", true},
+		{Thing3{"test", &num}, "name=test&num=", false},
+		{Thing3{"test", &num}, "name=test&num=0", true},
+		{Thing4{"test", num}, "name=test&num=", false},
+		{Thing4{"test", num}, "name=test&num=0", true},
+		{Thing1{"", &num}, "num=", false},
+		{Thing1{"", &num}, "num=0", true},
+		{Thing2{"", num}, "", false},
+		{Thing2{"", num}, "", true},
+		{Thing3{"", &num}, "name=&num=", false},
+		{Thing3{"", &num}, "name=&num=0", true},
+		{Thing4{"", num}, "name=&num=", false},
+		{Thing4{"", num}, "name=&num=0", true},
+	} {
+
+		var w bytes.Buffer
+		e := NewEncoder(&w)
+
+		if err := e.KeepZeros(c.z).Encode(c.b); err != nil {
+			t.Errorf("KeepZeros(%#v).Encode(%#v): %s", c.z, c.b, err)
+		} else if s := w.String(); c.s != s {
+			t.Errorf("KeepZeros(%#v).Encode(%#v)\n want (%#v)\n have (%#v)", c.z, c.b, c.s, s)
+		}
+	}
+}
